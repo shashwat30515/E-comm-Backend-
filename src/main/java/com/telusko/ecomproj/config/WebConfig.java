664ee4dcc42bebd1200c33e88fbfca616f989cee
Shashwat Singh <your-email@example.com>
2025-11-17 22:12:14 +0530

    add docker

---
+++ src/main/java/com/telusko/ecomproj/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.telusko.ecomproj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")              // <- change "*" to your Amplify URL in production
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+            }
+        };
+    }
+}
+
