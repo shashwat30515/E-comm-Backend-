fbe8d8e1f22a26f40bc7b95a117cf0681ad0e778
Shashwat Singh <your-email@example.com>
2025-11-20 00:08:32 +0530

    resolve conflicts

---
+++ src/main/java/com/telusko/ecomproj/config/GlobalCorsFilter.java
@@ -0,0 +1,28 @@
+package com.telusko.ecomproj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class GlobalCorsFilter {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(List.of("https://main.d4dlcci0bgt6u.amplifyapp.com"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
