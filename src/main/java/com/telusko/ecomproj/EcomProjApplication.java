669622b0ef19f1e012a41d06f94b030d4da36f43
Shashwat Singh <your-email@example.com>
2025-11-20 14:30:15 +0530

    resolve conflicts

--- src/main/java/com/telusko/ecomproj/EcomProjApplication.java
+++ src/main/java/com/telusko/ecomproj/EcomProjApplication.java
@@ -2,9 +2,7 @@ package com.telusko.ecomproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class EcomProjApplication {
@@ -13,20 +11,8 @@ public class EcomProjApplication {
         SpringApplication.run(EcomProjApplication.class, args);
 
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://main.d4dlcci0bgt6u.amplifyapp.com")
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("*")
-                        .allowCredentials(false);
-            }
-        };
+
     }
 
 
-}
+
